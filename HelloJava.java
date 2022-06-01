e6a5f30198cad471fa223a375f901cb0e4447cfd
Giuseppe Badagliacca <joebadagliacca@gmail.com>
2022-06-01 13:35:30 -0400

    java practice

---
+++ HelloJava.java
@@ -0,0 +1,12 @@
+public class HelloJava{
+  // this main class is where java starts running your code
+  public static void main(String[] args) {
+    System.out.println("Hello Java version II");
+
+    // commands that you need to run your app
+    // javac <file name>.java --> this complies your code
+
+    // java <file name> --> runs the code that has been complied
+
+  }
+}
