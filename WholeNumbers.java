eb911eb67f9bd15f006e9a0b62e8687623f0b77f
Giuseppe Badagliacca <joebadagliacca@gmail.com>
2022-06-01 16:31:24 -0400

    updated course progress

---
+++ WholeNumbers.java
@@ -0,0 +1,6 @@
+public class WholeNumber {
+    public static void main(String[] args) {
+        int numberOfFans = 5000;
+        System.out.println("There are " + numberOfFans + " number of fans at the Quidditch games.");
+    }
+}
---
+++ sectionTwo/Variables.java
@@ -0,0 +1,6 @@
+public class Variables {
+    public static void main(String[] args) {
+        int passengers = 20;
+        System.out.println(passengers);
+    }
+}
